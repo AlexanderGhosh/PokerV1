4058d59c4821c79b51480068637af7b946f2a6c0
AlexanderGhosh <ghoshalexander@gmail.com>
2021-09-09 17:41:04 +0100

    shoud now iuse herok port when deployed

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const port = 8000;
+const port = process.env.NODE_ENV | 8000;
 
 app.use(express.static(__dirname + '/static'));
 
