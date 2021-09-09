c1c6adb512eb17f1ea16413e8f46101a22dae959
AlexanderGhosh <ghoshalexander@gmail.com>
2021-09-09 17:13:18 +0100

    .

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const path = require('path');
 const app = express();
 const port = 8000;
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/static'));
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/src/index.html');
